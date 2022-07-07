8103e6135dfc7df1b15bde6b9cfccb5e9da60c45
G <108169022+eatyouradb@users.noreply.github.com>
2022-07-07 21:05:45 +0800

    7.7test2

---
+++ test2-while do.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int line = 0;
+	printf("学习C语言");
+	while (line<20000)
+	{
+		printf("每天敲代码\n");
+		line++;
+	}
+	printf("好offer\n");
+	return 0;
+}
