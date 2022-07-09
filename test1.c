007acb1a35026b0ac48d63e4a7fd711b28617444
G <108169022+eatyouradb@users.noreply.github.com>
2022-07-09 21:39:56 +0800

    Add files via upload
    数组sizeof

--- test1.c
+++ test1.c
@@ -1,15 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+#include <string.h>
 int main()
 {
-	int input = 0;
-	printf("你要好好学习吗? (好好学 1/天天躺平 0)>:");
-	scanf("%d", &input);
-	if(input == 1)
-		printf("成功上岸\n");
-	else
-	{
-		printf("天天搬砖\n");
-	}
+	int arr[10] = {0};
+	int sz = 0;
+	printf("%d\n", sizeof(arr));
+	sz=sizeof(arr) / sizeof(arr[0]);
+	printf("sz = %d\n", sz);
 	return 0;
 }
