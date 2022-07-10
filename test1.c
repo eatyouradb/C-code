aa93bfc445d00bf520e9ea1c76542904d41bf404
G <108169022+eatyouradb@users.noreply.github.com>
2022-07-10 20:17:26 +0800

    函数表达两数中较大值

--- test1.c
+++ test1.c
@@ -1,12 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-#include <string.h>
+int Max(int x, int y)
+{
+	if (x > y)
+		return x;
+	else
+		return y;
+}
 int main()
 {
-	int arr[10] = {0};
-	int sz = 0;
-	printf("%d\n", sizeof(arr));
-	sz=sizeof(arr) / sizeof(arr[0]);
-	printf("sz = %d\n", sz);
+	int a = 10;
+	int b = 20;
+	int max = Max(a, b);
+	/*if (a > b)
+		printf("a is bigger than b");
+	else
+		printf("b is bigger than a");*/
+	printf("max=%d\n", max);
 	return 0;
 }
