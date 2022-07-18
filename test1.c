a40a2834aa5fd3d969b94a02272ea3ed510acf99
G <108169022+eatyouradb@users.noreply.github.com>
2022-07-18 16:24:20 +0800

    选择语句练习1

--- test1.c
+++ test1.c
@@ -1,21 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-int Max(int x, int y)
-{
-	if (x > y)
-		return x;
-	else
-		return y;
-}
 int main()
 {
-	int a = 10;
-	int b = 20;
-	int max = Max(a, b);
-	/*if (a > b)
-		printf("a is bigger than b");
+	int age = 55;
+	if (age < 18)
+		printf("未成年\n");
+	else if (age >= 18 && age < 28)
+		printf("青年\n");
 	else
-		printf("b is bigger than a");*/
-	printf("max=%d\n", max);
+		printf("成年\n");
 	return 0;
 }
