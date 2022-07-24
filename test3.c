03e9d5bc2885e442a251e3e3d0aa1e4ab14ee697
G <108169022+eatyouradb@users.noreply.github.com>
2022-07-24 22:09:15 +0800

    密码练习1

--- test3.c
+++ test3.c
@@ -1,12 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+#include <string.h>
 int main()
 {
-	int i = 1;
-	while (i <= 10)
+	int i = 0;
+	char password[20] = {0};
+	for (i = 0; i < 3; i++)
 	{
-		printf("%d\n", i);
-		i++;
+		printf("password:> ");
+		scanf("%s", password);
+		if (strcmp(password, "555") == 0)
+		{
+			printf("password correct\n");
+			break;
+		}
+		else
+		{
+			printf("Password error\n");
+		}
+	}
+	if (3 == i)
+	{
+		printf("Three attempts failed, the program has been exited\n");
 	}
 	return 0;
 }
