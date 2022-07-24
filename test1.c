74e5320f371bccc7a8e02743878b68c8c41b132e
G <108169022+eatyouradb@users.noreply.github.com>
2022-07-24 20:55:16 +0800

    二分查找

--- test1.c
+++ test1.c
@@ -2,15 +2,33 @@
 #include <stdio.h>
 int main()
 {
-	int i = 0;
-	int n = 0;
-	int ret = 1;
-	printf("Please enter a number:>");
-	scanf("%d", &n);
-	for (i = 1; i <= n; i++)
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int left = 0;
+	int right = sz-1;
+	int k = 0;
+	printf("Please enter the value you want to find:>");
+	scanf("%d", &k);
+	while (left <= right)
 	{
-		ret = ret * i;	
+		int mid = (left + right) / 2;
+		if (arr[mid] < k)
+		{
+			left = mid + 1;
+		}
+		else if (arr[mid] > k)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			printf("The position of this value is in %d\n", mid);
+			break;
+		}
 	}
-	printf("Factorial = %d\n", ret);
+	if (left > right)
+	{
+		printf("The value cannot be found\n");
+	}
 	return 0;
 }
