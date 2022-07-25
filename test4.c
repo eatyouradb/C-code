35cd4041b2a8531378bbd8e06c0b67c66de42553
G <108169022+eatyouradb@users.noreply.github.com>
2022-07-25 20:55:30 +0800

    Update test4.c

--- test4.c
+++ test4.c
@@ -4,16 +4,20 @@ int main()
 {
 	int i = 0;
 	int r = 0;
+	int count = 0;
 	for (i = 1000; i <= 2000; i++)
 	{
 		if (i % 4==0 && i % 100!=0)
 		{
 			printf("%d ", i);
+			count++;
 		}
 		else if (i % 400==0)
 		{
 			printf("%d ", i);
+			count++;
 		}
 	}
+	printf("\ncount=%d", count);
 	return 0;
 }
