7f7ff966b9e8791dd68d73859168c28178ad6617
G <108169022+eatyouradb@users.noreply.github.com>
2022-07-25 21:36:39 +0800

    100~200素数 优化.ver

--- test4.c
+++ test4.c
@@ -1,18 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+#include <math.h>
 int main()
 {
 	int i = 0;
-	int r = 0;
 	int count = 0;
-	for (i = 1000; i <= 2000; i++)
+	int j = 0;
+	for (i = 100; i <= 200; i++)
 	{
-		if (i % 4==0 && i % 100!=0)
+		for (j = 2; j <= sqrt(i); j++)//若i能被整除，则i=a*b,那么a&b<=sqrt(i)
 		{
-			printf("%d ", i);
-			count++;
-		}
-		else if (i % 400==0)
+			if (0==i % j)
+			{
+				break;
+			}
+		}//1.被整除  2.j都不能整除i
+		if (j > sqrt(i))
 		{
 			printf("%d ", i);
 			count++;
