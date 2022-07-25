388a7fdb2de80c72f10d58161fa33aaf62863ecf
G <108169022+eatyouradb@users.noreply.github.com>
2022-07-25 20:53:05 +0800

    1000~2000的闰年

---
+++ test4.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+int main()
+{
+	int i = 0;
+	int r = 0;
+	for (i = 1000; i <= 2000; i++)
+	{
+		if (i % 4==0 && i % 100!=0)
+		{
+			printf("%d ", i);
+		}
+		else if (i % 400==0)
+		{
+			printf("%d ", i);
+		}
+	}
+	return 0;
+}
