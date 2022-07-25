1b0d8a942fe0f09e12c064b5f2dc586cb705f861
G <108169022+eatyouradb@users.noreply.github.com>
2022-07-25 20:15:37 +0800

    1~100内3的倍数

--- test2.c
+++ test2.c
@@ -1,26 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-#include <string.h>
-#include <windows.h>
-#include <stdlib.h>
-
 int main()
 {
-	char arr1[] = { "I love c language\0" };
-	char arr2[] = { "#################" };
-	int left = 0;
-	int right = strlen(arr1)-1;
-	while (left <= right)
+	int i = 0;
+	for (i = 1; i < 100; i++)
 	{
-		arr2[left] = arr1[left];
-		arr2[right] = arr1[right];
-		printf("%s\n", arr2);
-		Sleep(1000);//休息1000毫秒=1s
-		system("cls");//清空屏幕
-		left++;
-		right--;
+		if (0 == i % 3)
+		{
+			printf("%d ", i);
+		}
 	}
-	printf("%s\n", arr2);
 	return 0;
-
 }
