923a8e2873abc3efc99812ab7006b4e92e3ea09c
G <108169022+eatyouradb@users.noreply.github.com>
2022-07-27 20:30:07 +0800

    十个数中最大数

--- test3.c
+++ test3.c
@@ -1,17 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+
 int main()
 {
-	int n = 0;
-	int m = 0;
-	int r = 0;
-	scanf("%d%d", &n, &m);
-	while ( r = n % m )
+	char arr[] = { 1,2,4,3,5,11,7,8,9,10 };
+	int i = 0;
+	int max = 0;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	for (i = 0; i <= sz; i++)
 	{
-		n = m;
-		m = r;
+		if (arr[i] > max)
+		{
+			max = arr[i];
+		}
 	}
-	printf("The greatest common divisor is: %d\n",m);
+	printf("%d", max);
 	return 0;
-	
 }
