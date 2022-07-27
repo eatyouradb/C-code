6b2c77cebac3d87637bda676bcb85ac57a2282b7
G <108169022+eatyouradb@users.noreply.github.com>
2022-07-27 21:03:08 +0800

    99乘法表

--- test4.c
+++ test4.c
@@ -1,26 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-#include <math.h>
 int main()
 {
 	int i = 0;
-	int count = 0;
-	int j = 0;
-	for (i = 100; i <= 200; i++)
+	int n = 0;
+	for (i = 1; i < 10; i++)
 	{
-		for (j = 2; j <= sqrt(i); j++)//若i能被整除，则i=a*b,那么a&b<=sqrt(i)
+		for (n = 1; n <= i; n++)
 		{
-			if (0==i % j)
-			{
-				break;
-			}
-		}//1.被整除  2.j都不能整除i
-		if (j > sqrt(i))
-		{
-			printf("%d ", i);
-			count++;
+			printf("%d*%d=%-2d ", i, n, i*n);
 		}
+		printf("\n");
 	}
-	printf("\ncount=%d", count);
 	return 0;
 }
