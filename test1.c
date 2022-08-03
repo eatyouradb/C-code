9edd8502e577d18c38a8afb9f0ff1260f7f2ba6f
G <108169022+eatyouradb@users.noreply.github.com>
2022-08-03 20:31:06 +0800

    字符串函数strcpy练习

--- test1.c
+++ test1.c
@@ -1,66 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-void game()
-{
-	int ran = 0;
-	int guess = 0;
-	int count = 0;
-	ran = rand()%100+1;
-	
-	while (1)
-	{
-		printf("Please guess>:");
-		scanf("%d", &guess);
-		if (guess < ran)
-		{
-			printf("small\n");
-			count++;
-		}
-		else if (guess > ran)
-		{
-			printf("big\n");
-			count++;
-		}
-		else
-		{
-			printf("You guessed right\n");
-			break;
-		}
-		
-	}
-	printf("Guess times=%d\n", count);/*printf("%d\n",ran);*/
-}
-void title()
-{
-	printf("********************************************\n");
-	printf("***  1 continue   *******   0  exit   ******\n");
-	printf("********************************************\n");
-}
+#include <string.h>
 int main()
 {
-	srand((unsigned int)time(NULL));
-	int input = 0;
-	do
-	{
-		title();
-		printf("Please enter>:");
-		scanf("%d", &input);
-		switch(input)
-		{
-		case 0:
-			printf("game over\n");
-			break;
-		case 1:
-			game();
-			break;
-		default:
-			printf("Input error\n");
-			break;
-		}
-	} 
-	while (input);
+	char arr1[] = { "bit" };
+	char arr2[20] = { "##########" };
+	strcpy(arr2, arr1);
+	printf("%s", arr2);
 	return 0;
 }
