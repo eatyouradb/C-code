f913be4a921086d16841de782d7007bc3446020c
G <108169022+eatyouradb@users.noreply.github.com>
2022-08-04 22:22:39 +0800

    函数——二分法

--- test4.c
+++ test4.c
@@ -1,16 +1,45 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+int binary_search(int arr[],int k,int sz)
+{
+	int left = 0;
+	/*int sz = sizeof(arr) / sizeof(arr[0]);*/
+	//sizeof(arr)传递的是arr的首元素的下标，sizeof(arr)==4
+	int right = sz-1;
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		if (arr[mid] < k)
+		{
+			left = mid + 1;
+			
+		}
+		else if (arr[mid] > k)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			return mid;
+		}
+	}
+	return -1;
+}
 int main()
 {
-	int i = 0;
-	int n = 0;
-	for (i = 1; i < 10; i++)
+	int arr[] = { 1,2,3,5,8,9,12,13};
+	int k = 0;
+	printf("Please enter a number>:");
+	scanf("%d", &k);
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int ret = binary_search(arr, k, sz);
+	if (-1 == ret)
 	{
-		for (n = 1; n <= i; n++)
-		{
-			printf("%d*%d=%-2d ", i, n, i*n);
-		}
-		printf("\n");
+		printf("The number is not in the array\n");
+	}
+	else
+	{
+		printf("The number is in the array,Subscript is=%d\n",ret);
 	}
 	return 0;
 }
