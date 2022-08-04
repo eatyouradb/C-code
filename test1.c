147c99268a3e314d04acae47cc32cd4f103084f9
G <108169022+eatyouradb@users.noreply.github.com>
2022-08-04 17:09:52 +0800

    用函数交换两个整数（内存/指针）

--- test1.c
+++ test1.c
@@ -1,11 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-#include <string.h>
+void Swap(int* pa, int* pb)
+{
+	int tmp = 0;
+	tmp = *pa;
+	*pa = *pb;
+	*pb = tmp;
+}
 int main()
 {
-	char arr1[] = { "bit" };
-	char arr2[20] = { "##########" };
-	strcpy(arr2, arr1);
-	printf("%s", arr2);
+	int a = 0;
+	int b = 0;
+	printf("Please enter two numbers>:");
+	scanf("%d%d", &a, &b);
+	Swap(&a, &b);
+	printf("%d,%d", a, b);
 	return 0;
 }
