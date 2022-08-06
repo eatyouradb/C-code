5c50eec299b4534482631788c8255bda5418fad5
G <108169022+eatyouradb@users.noreply.github.com>
2022-08-06 20:12:59 +0800

    函数递归——字符串长度

--- test2.c
+++ test2.c
@@ -1,52 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-//int is_prime(int x)
-//{
-//	int i = 0;
-//	for (i = 2; i < x; i++)
-//	{
-//		if (0 == x % i)
-//		{
-//			printf("This number is prime\n");
-//			break;
-//		}
-//	}
-//	if (i == x)
-//	{
-//		printf("This number is not prime\n");
-//	}
-//}
-//int main()
-//{
-//	int a = 0;
-//	printf("Please enter a number>:");
-//	scanf("%d", &a);
-//	is_prime(a);
-//	return 0;
-//}
-#include <math.h>
-int is_prime(int x)
+
+int length(char* arr)
 {
-	int n = 0;
-	for (n = 2; n <= sqrt(x); n++)
+	if (*arr != '\0')
 	{
-		if (0 == x % n)
-		{
-			return 0;/*相比break，return 0效率强，直接结束函数*/
-		}
+		return 1 + length(arr + 1);
 	}
-	return 1;
-} 
+	else
+	{
+		return 0;
+	}
+}
 int main()
 {
-	int i = 0;
-	int n = 0;
-	for (i = 100; i <= 200; i++)
-	{
-		if (1 == is_prime(i))
-		{
-			printf("%d\n",i);
-		}
-	}
+	char arr[] = "bit";
+	int le = length(arr);
+	printf("%d\n", le);
 	return 0;
 }
