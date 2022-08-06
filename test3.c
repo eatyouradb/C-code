f1c1b1180b7d948d75af5ff4f4db1333d8a4596b
G <108169022+eatyouradb@users.noreply.github.com>
2022-08-06 21:01:28 +0800

    函数递归——阶乘

--- test3.c
+++ test3.c
@@ -1,45 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-//int Leap_year(int x)
-//{
-//	if (x % 4 == 0 && x % 100 == !0 || 0 == x % 400)
-//	{
-//		printf("This year is a leap year\n");
-//	}
-//	else
-//	{
-//		printf("This year is a leap year\n");
-//	}
-//}
-//int main()
-//{
-//	int i = 0;
-//	printf("Please enter the year>:");
-//	scanf("%d", &i);
-//	Leap_year(i);
-//
-//	return 0;
-//}Ì«´ì£¬²»Õû½à¸É¾»
-int Leap_year(int x)
+int Fac(int x)
 {
-	if ((0 == x % 4 && x % 100 != 0) || 0 == x % 400)
+	if (x <= 1)
 	{
 		return 1;
 	}
 	else
 	{
-		return 0;
+		/*for (i = 1; i <= x; i++)
+		{
+			ret *= i;
+		}*/
+		return x * Fac(x - 1);
 	}
 }
 int main()
 {
-	int year = 0;
-	for (year = 1000; year <= 2000; year++)
-	{
-		if (1 == Leap_year(year))
-		{
-			printf("%d\n", year);
-		}
-	}
-	return 0;
+	int n = 0;
+	scanf("%d", &n);
+	int f = Fac(n);
+	printf("%d\n", f);
 }
