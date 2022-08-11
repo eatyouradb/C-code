764fb29ca743e80469a9f4b5df1c0bf8c3680147
G <108169022+eatyouradb@users.noreply.github.com>
2022-08-11 17:56:43 +0800

    打印字符串与整型数组

--- test1.c
+++ test1.c
@@ -1,17 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-void print(int n)
-{
-	if (n > 9)
-	{
-		print(n / 10);
-	}
-	printf("%d ", n % 10);
-}
+#include <string.h>
 int main()
 {
-	unsigned int num = 0;
-	scanf("%d", &num);
-	print(num);
+	/*char arr1[] = "abcdef";
+	int i = 0;
+	int len = strlen(arr1);
+	for (i = 0; i < len; i++)
+	{
+		printf("%c ", arr1[i]);
+	}*/
+	int arr2[] = { 1,2,3,4,5,6,7,8,9,0 };
+	int sz = sizeof(arr2) / sizeof(arr2[0]);
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr2[i]);
+	}
 	return 0;
 }
