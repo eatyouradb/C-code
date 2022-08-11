5d6bb22781bcfade526f755068def01b7354a4bc
G <108169022+eatyouradb@users.noreply.github.com>
2022-08-11 21:00:20 +0800

    数组冒泡排序

--- tset3.c
+++ tset3.c
@@ -1,28 +1,34 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-void bubble_sort(int arr[],int sz)
+void bubble_sort(int arr[], int sz)
 {
-	int i = 0;
+	int i = 0;//跑多少趟
+	int flag = 1;//本躺是否有序
 	for (i = 0; i < sz - 1; i++)
 	{
-		int j = 0;
-		for (j = 0; j < sz - i - 1; j++)
+		int j = 0;//交换多少对儿
+		for (j = 0; j < sz - 1 - i; j++)
 		{
-			if (arr[j] > arr[j + 1])
+			if (arr[j] < arr[j + 1])
 			{
-				int n = arr[j];
+				int tmp = arr[j];
 				arr[j] = arr[j + 1];
-				arr[j + 1] = n;
+				arr[j + 1] = tmp;
+				flag = 0;
 			}
+		}
+		if (1 == flag)
+		{
+			break;
 		}
 	}
 }
 int main()
 {
 	int arr[] = { 10,11,8,7,6,5,4,3,2,1 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
 	int i = 0;
-	bubble_sort(arr,sz);
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	bubble_sort(arr, sz);
 	for (i = 0; i < sz; i++)
 	{
 		printf("%d ", arr[i]);
