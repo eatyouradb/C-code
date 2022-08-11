12055005ab90c8873ed452e9e7c47b3c3de151e4
G <108169022+eatyouradb@users.noreply.github.com>
2022-08-11 21:04:50 +0800

    Update tset3.c

--- tset3.c
+++ tset3.c
@@ -3,9 +3,9 @@
 void bubble_sort(int arr[], int sz)
 {
 	int i = 0;//跑多少趟
-	int flag = 1;//本躺是否有序
 	for (i = 0; i < sz - 1; i++)
 	{
+		int flag = 1;//本躺是否有序
 		int j = 0;//交换多少对儿
 		for (j = 0; j < sz - 1 - i; j++)
 		{
