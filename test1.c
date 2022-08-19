6edfd4f614f406e15723d770ccd8e5154ac6326c
G <108169022+eatyouradb@users.noreply.github.com>
2022-08-19 18:06:48 +0800

    函数练习a

--- test1.c
+++ test1.c
@@ -1,16 +1,43 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+void Init(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		arr[i] = 0;
+	}
+}
+void Print(int arr[], int sz)
+{
+	int i = 0;
+	for (i = 0; i < sz; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+}
+void Reverse(int arr[], int sz)
+{
+	int left = 0;
+	int right = sz - 1;
+	while (left < right)
+	{
+		int tmp = 0;
+		tmp = arr[left];
+		arr[left] = arr[right];
+		arr[right] = tmp;
+		left++;
+		right--;
+	}
+}
 int main()
 {
-	int a = 5;
-	int b = 3;
-	printf("Before a=%d,b=%d\n", a, b);
-	/*a = a + b;
-	b = a - b;
-	a = a - b;*/
-	a = a ^ b;
-	b = a ^ b;
-	a = a ^ b;
-	printf("After a=%d,b=%d\n", a, b);
+	int arr[] = { 0,1,2,3,4,5 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	/*Init(arr, sz);*/
+	Print(arr, sz);
+	Reverse(arr, sz);
+	Print(arr, sz);
 	return 0;
 }
