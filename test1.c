a0a9fbc126fc5c3eb8b77e7885dd0b97bf0cd7c1
G <108169022+eatyouradb@users.noreply.github.com>
2022-08-20 18:12:38 +0800

    二进制1的个数练习

--- test1.c
+++ test1.c
@@ -13,6 +13,19 @@ int count_bit_one(unsigned int a)//负数换成无符号数
 	}
 	return i;
 }
+//int count_bit_one(int a)
+//{
+//	int i = 0;
+//	int count = 0;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if (1 == (a >> i) & 1)
+//		{
+//			count++;
+//		}
+//	}
+//	return count;
+//}
 int main()
 {
 	int a = 0;
