2b2c8acdf10a33e54d54193521648b05f1fc91e0
G <108169022+eatyouradb@users.noreply.github.com>
2022-08-21 17:12:36 +0800

    逆序字符串

--- test2.c
+++ test2.c
@@ -1,24 +1,50 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-int get_count_one(int m, int n)
+int my_strlen(char* arr)
 {
-	int tmp = 0;
 	int count = 0;
-	tmp = m ^ n;
-	while (tmp)
+	while(*arr)
 	{
-		tmp = tmp & (tmp - 1);
 		count++;
+		arr++;
 	}
 	return count;
 }
+//void reverse_string(char arr[])
+//{
+//	int left = 0;
+//	int right = my_strlen(arr) - 1;
+//	while (left < right)
+//	{
+//		int tmp = 0;
+//		tmp = arr[left];
+//		arr[left] = arr[right];
+//		arr[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//}
 
+void reverse_string(char arr[])
+{
+	char tmp = arr[0];
+
+	int len = my_strlen(arr);
+	arr[0] = arr[len - 1];
+
+	arr[len - 1] = '\0';
+	if (my_strlen(arr + 1) >= 2)
+	{
+		reverse_string(arr + 1);
+	}
+
+	arr[len - 1] = tmp;
+}
 int main()
 {
-	int m = 0;
-	int n = 0;
-	scanf("%d%d", &m, &n);
-	int count = get_count_one(m, n);
-	printf("count=%d\n", count);
+	char arr[] = "abcefg";
+	/*int sz = sizeof(arr) / sizeof(arr[0]);*/
+	reverse_string(arr);
+	printf("%s\n", arr);
 	return 0;
 }
