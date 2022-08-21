a631d9068d83c9758a571a077365c33f7a3a47bb
G <108169022+eatyouradb@users.noreply.github.com>
2022-08-21 16:25:38 +0800

    乘法表

--- test1.c
+++ test1.c
@@ -1,46 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-//int count_bit_one(unsigned int a)//负数换成无符号数
-//{
-//	int i = 0;
-//	while (a)
-//	{
-//		if (1 == a % 2)
-//		{
-//			i++;
-//		}
-//		a = a / 2;
-//	}
-//	return i;
-//}
-//int count_bit_one(int a)
-//{
-//	int i = 0;
-//	int count = 0;
-//	for (i = 0; i < 32; i++)
-//	{
-//		if (1 == (a >> i) & 1)
-//		{
-//			count++;
-//		}
-//	}
-//	return count;
-//}
-int count_bit_one(int a)
+void Pithy_table(int x)
 {
-	int count = 0;
-	while (a)
+	int i = 0;
+	for (i = 1; i <= x; i++)
 	{
-		a = a & (a - 1);
-		count++;
+		int j = 0;
+		for (j = 1; j <= i; j++)
+		{
+			printf("%d*%d=%-2d ", j, i, i * j);
+		}
+		printf("\n");
 	}
-	return count;
 }
 int main()
 {
-	int a = 0;
-	scanf("%d", &a);
-	int count = count_bit_one(a);
-	printf("%d", count);
+	int n = 0;
+	scanf("%d", &n);
+	Pithy_table(n);
 	return 0;
 }
