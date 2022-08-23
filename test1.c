706bf65caebc4e13a404c3f63079246b32c5ba13
G <108169022+eatyouradb@users.noreply.github.com>
2022-08-23 20:24:04 +0800

    判断死循环

--- test1.c
+++ test1.c
@@ -1,22 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-void Pithy_table(int x)
-{
-	int i = 0;
-	for (i = 1; i <= x; i++)
-	{
-		int j = 0;
-		for (j = 1; j <= i; j++)
-		{
-			printf("%d*%d=%-2d ", j, i, i * j);
-		}
-		printf("\n");
-	}
-}
 int main()
 {
-	int n = 0;
-	scanf("%d", &n);
-	Pithy_table(n);
+	int i = 0;
+	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+	for (i = 1; i <= 12; i++)
+	{
+		printf("haha\n");
+		arr[i] = 0;
+	}
 	return 0;
 }
+//i与arr都为局部变量，存储在内存中的栈区
+//栈区默认是先使用高地址，再使用低地址，所以i的地址比arr的地址高
+//因为arr是数组，数组默认是随下标的增加，地址由低到高变化
+//当arr持续越界时，地址逐渐升高，就有可能遇到i的地址
+//这时改变arr的值，i的值也变，程序就可能陷入死循环
