03f042b83ad13a8441907a22d43b0ea3b628256f
G <108169022+eatyouradb@users.noreply.github.com>
2022-08-25 20:14:39 +0800

    函数实现strcpy

--- test1.c
+++ test1.c
@@ -1,20 +1,40 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-void my_strcpy(char* dest, char* src)
+#include <assert.h>
+//void my_strcpy(char* dest, char* src)
+//{
+//	while (*src!=0)
+//	{
+//		*dest = *src;
+//		src++;
+//		dest++;
+//	}
+//	*dest = *src;//'\0'
+//}
+// 
+//void my_strcpy(char* dest, char* src)
+//{
+//	while (*dest++ = *src++)
+//	{
+//		;
+//	}
+//}
+
+char* my_strcpy(char* dest, const char* src)//源头数据*src不能被改变//返回值优化
 {
-	while (*src!=0)
+	int* ret = dest;
+	assert(dest != NULL);//断言，优化
+	assert(src != NULL);
+	while (*dest++ = *src++)//优化
 	{
-		*dest = *src;
-		src++;
-		dest++;
+		;
 	}
-	*dest = *src;
+	return ret;
 }
 int main()
 {
 	char arr1[] = "############";
 	char arr2[] = "hello";
-	my_strcpy(arr1, arr2);
-	printf("%s\n", arr1);
+	printf("%s\n", my_strcpy(arr1, arr2));
 	return 0;
 }
