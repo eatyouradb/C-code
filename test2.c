a133ccc4599b821de03d4fcf8d0d558d106056f8
G <108169022+eatyouradb@users.noreply.github.com>
2022-08-25 20:28:28 +0800

    函数实习strlen.优化版

--- test2.c
+++ test2.c
@@ -1,50 +1,21 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-int my_strlen(char* arr)
+#include <assert.h>
+int my_strlen(const char* arr)
 {
 	int count = 0;
-	while(*arr)
+	assert(arr != NULL);
+	while (*arr != '\0')
 	{
 		count++;
 		arr++;
 	}
 	return count;
 }
-//void reverse_string(char arr[])
-//{
-//	int left = 0;
-//	int right = my_strlen(arr) - 1;
-//	while (left < right)
-//	{
-//		int tmp = 0;
-//		tmp = arr[left];
-//		arr[left] = arr[right];
-//		arr[right] = tmp;
-//		left++;
-//		right--;
-//	}
-//}
-
-void reverse_string(char arr[])
-{
-	char tmp = arr[0];
-
-	int len = my_strlen(arr);
-	arr[0] = arr[len - 1];
-
-	arr[len - 1] = '\0';
-	if (my_strlen(arr + 1) >= 2)
-	{
-		reverse_string(arr + 1);
-	}
-
-	arr[len - 1] = tmp;
-}
 int main()
 {
-	char arr[] = "abcefg";
-	/*int sz = sizeof(arr) / sizeof(arr[0]);*/
-	reverse_string(arr);
-	printf("%s\n", arr);
+	char arr[] = "abcde";
+	int len = my_strlen(arr);
+	printf("%d\n", len);
 	return 0;
 }
