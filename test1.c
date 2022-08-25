61d6e2e63588aab53a90f669d4e1063d7e50840d
G <108169022+eatyouradb@users.noreply.github.com>
2022-08-25 17:38:54 +0800

    实现strcpy功能的函数

--- test1.c
+++ test1.c
@@ -1,18 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+void my_strcpy(char* dest, char* src)
+{
+	while (*src!=0)
+	{
+		*dest = *src;
+		src++;
+		dest++;
+	}
+	*dest = *src;
+}
 int main()
 {
-	int i = 0;
-	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
-	for (i = 1; i <= 12; i++)
-	{
-		printf("haha\n");
-		arr[i] = 0;
-	}
+	char arr1[] = "############";
+	char arr2[] = "hello";
+	my_strcpy(arr1, arr2);
+	printf("%s\n", arr1);
 	return 0;
 }
-//i与arr都为局部变量，存储在内存中的栈区
-//栈区默认是先使用高地址，再使用低地址，所以i的地址比arr的地址高
-//因为arr是数组，数组默认是随下标的增加，地址由低到高变化
-//当arr持续越界时，地址逐渐升高，就有可能遇到i的地址
-//这时改变arr的值，i的值也变，程序就可能陷入死循环
