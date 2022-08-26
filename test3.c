329626f9a1b353cc1885bd5d2367628fa17d9aa6
G <108169022+eatyouradb@users.noreply.github.com>
2022-08-26 21:35:49 +0800

    二进制练习2

--- test3.c
+++ test3.c
@@ -1,20 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-int calculate(unsigned int n)
-{
-	if (n>9)
-	{
-		return calculate(n / 10) + n % 10;
-	}
-	else
-	{
-		return n;
-	}
-}
+#include <string.h>
 int main()
 {
-	unsigned int n = 0;
-	scanf("%d", &n);
-	printf("%d", calculate(n));
+	char a[1000];
+	int i = 0;
+	for (i = 0; i < 1000; i++)
+	{
+		a[i] = -1 - i;
+	}
+	//a为char类型，存放大小为-128——127
+	//数组a中的元素为-1,-2,-3,...,-1000
+	//当a[i]=-128时，下一元素-129应转化为-128——127中的数，即127,126,...,1,0,-1,...
+	//strlen遇到0停止，第一个0前有1——127+（-128）——（-1）个数（127+128）=255
+	printf("%d", strlen(a));
 	return 0;
 }
