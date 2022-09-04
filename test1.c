1cf2ad4958afd27223b09d41125c1b936d418b84
G <108169022+eatyouradb@users.noreply.github.com>
2022-09-05 06:59:09 +0800

    sizeof大小练习

--- test1.c
+++ test1.c
@@ -1,44 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-#include <stdlib.h>
-//void bubble_sort(int arr[], int sz)
-//{
-//	int i = 0;
-//	for (i = 0; i < sz - 1; i++)
-//	{
-//		int j = 0;
-//		for (j = 0; j < sz - 1 - i; j++)
-//		{
-//			if (arr[j] > arr[j + 1])
-//			{
-//				int tmp = arr[j];
-//				arr[j] = arr[j + 1];
-//				arr[j + 1] = tmp;
-//			}
-//		}
-//	}
-//}
-int cmp(const void* e1, const void* e2)
-{
-	return *(int*)e1 - *(int*)e2;
-	//将e1,e2强制转换成int *类型
-	//小于返回负数，等于返回0，大于返回正数
-}
-void test()
-{
-	int arr[5] = { 5,4,3,2,1 };
-	int i = 0;
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	qsort(arr, sz, sizeof(arr[0]), cmp);
-	/*bubble_sort (arr, sz);*/
-	for (i = 0; i < sz; i++)
-	{
-		printf("%d ", arr[i]);
-	}
-	printf("\n");
-}
 int main()
 {
-	test();
+	int a[] = { 5,4,3,2 };
+	printf("%d\n", sizeof(a));        //16 sizeof(数组名)计算的是整个数组大小
+	printf("%d\n", sizeof(a + 0));    //4/8 数组名这里表示的是首元素的值，a+0为首元素地址
+	printf("%d\n", sizeof(*a));       //4/8 对首元素地址进行解引用操作，得到首元素的值，值为整形
+	printf("%d\n", sizeof(a + 1));    //4/8 第二个元素的地址
+	printf("%d\n", sizeof(a[1]));     //4/8 第二个元素的大小
+	printf("%d\n", sizeof(&a));       //4/8 a整个数组的地址，因为是地址所以大小还是4/8
+	printf("%d\n", sizeof(*&a));      //16 &a是数组的地址，解引用操作访问的整个数组
+	printf("%d\n", sizeof(&a + 1));   //4/8 &a+1虽然地址跳过整个数组，但因为是地址所以大小还是4/8
+	printf("%d\n", sizeof(&a[0]));    //4/8 首元素地址
+	printf("%d\n", sizeof(&a[0] + 1));//4/8 第二个元素的地址
 	return 0;
 }
