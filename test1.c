b9d62a63f3038a3691c98972ba7b3ec2f75337e2
G <108169022+eatyouradb@users.noreply.github.com>
2022-09-06 17:36:28 +0800

    实现strcat功能的函数

--- test1.c
+++ test1.c
@@ -1,17 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+#include <assert.h>
+
+char* my_strcat(char* arr1, const char* arr2)
+{
+	char* ret = arr1;
+	assert(arr1 && arr2);
+	while (*arr1 != 0)
+	{
+		arr1++;
+	}
+	while (*arr1++ = *arr2++)
+	{
+		;
+	}
+	return ret;
+}
 int main()
 {
-	int a[] = { 5,4,3,2 };
-	printf("%d\n", sizeof(a));        //16 sizeof(数组名)计算的是整个数组大小
-	printf("%d\n", sizeof(a + 0));    //4/8 数组名这里表示的是首元素的值，a+0为首元素地址
-	printf("%d\n", sizeof(*a));       //4/8 对首元素地址进行解引用操作，得到首元素的值，值为整形
-	printf("%d\n", sizeof(a + 1));    //4/8 第二个元素的地址
-	printf("%d\n", sizeof(a[1]));     //4/8 第二个元素的大小
-	printf("%d\n", sizeof(&a));       //4/8 a整个数组的地址，因为是地址所以大小还是4/8
-	printf("%d\n", sizeof(*&a));      //16 &a是数组的地址，解引用操作访问的整个数组
-	printf("%d\n", sizeof(&a + 1));   //4/8 &a+1虽然地址跳过整个数组，但因为是地址所以大小还是4/8
-	printf("%d\n", sizeof(&a[0]));    //4/8 首元素地址
-	printf("%d\n", sizeof(&a[0] + 1));//4/8 第二个元素的地址
+	char arr1[30] = "abcd";
+	char arr2[] = "1234";
+	my_strcat(arr1, arr2);
+	printf("%s\n", arr1);
 	return 0;
 }
