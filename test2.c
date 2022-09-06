c40b503c7910425567c7451a6a1d28de796ae150
G <108169022+eatyouradb@users.noreply.github.com>
2022-09-06 17:58:35 +0800

    实现strcmp功能的函数

--- test2.c
+++ test2.c
@@ -1,51 +1,37 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-#include <string.h>
-
-int cmp(const void* e1, const void* e2)
+int my_strcmp(char* arr1, char* arr2)
 {
-	return *(int*)e1 - *(int*)e2;
-}
-void swap(char* buf1, char* buf2,int width)
-{
-	int i = 0;
-	for (i = 0; i < width; i++)
+	while (*arr1 == *arr2)
 	{
-		char tmp = *buf1;
-		*buf1 = *buf2;
-		*buf2 = tmp;
-		buf1++;
-		buf2++;
-	}
-}
-int bubble_sort(void* base, int sz, int width, int (*cmp)(const void* e1, const void* e2))
-{
-	int i = 0;
-	for (i = 0; i < sz - 1; i++)
-	{
-		int j = 0;
-		for (j = 0; j < sz - 1 - i; j++)
+		if ('\0' == *arr1)
 		{
-			if (cmp((char*)base + j * width, (char*)base + (j + 1) * width) > 0)
-			{
-				swap((char*)base + j * width, (char*)base + (j + 1) * width, width);
-			}
+			return 0;
 		}
+		arr1++;
+		arr2++;
 	}
-}
-void test()
-{
-	int arr[5] = { 5,4,3,2,1 };
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	int i = 0;
-	bubble_sort(arr, sz, sizeof(arr[0]), cmp);
-	for (i = 0; i < sz ; i++)
-	{
-		printf("%d", arr[i]);
-	}
+	if (*arr1 < *arr2)
+		return -1;
+	else
+		return 1;
 }
 int main()
 {
-	test();
+	char arr1[] = "abcdef";
+	char arr2[] = "abght";
+	int ret = my_strcmp(arr1, arr2);
+	if (ret < 0)
+	{
+		printf("arr1<arr2\n");
+	}
+	else if (ret > 0)
+	{
+		printf("arr1>arr2\n");
+	}
+	else
+	{
+		printf("arr1=arr2\n");
+	}
 	return 0;
 }
