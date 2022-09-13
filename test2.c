5fcb9751511673416d82c0c7892621a6e0b7ddc3
G <108169022+eatyouradb@users.noreply.github.com>
2022-09-13 20:25:56 +0800

    字符转化函数练习

--- test2.c
+++ test2.c
@@ -1,38 +1,18 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-int my_strcmp(char* arr1, char* arr2)
-{
-	while (*arr1 == *arr2)
-	{
-		if ('\0' == *arr1)
-		{
-			return 0;
-		}
-		arr1++;
-		arr2++;
-	}
-	/*if (*arr1 < *arr2)
-		return -1;
-	else
-		return 1;*/
-	return (*arr1 - *arr2);
-}
+#include <ctype.h>
 int main()
 {
-	char arr1[] = "abcdef";
-	char arr2[] = "abght";
-	int ret = my_strcmp(arr1, arr2);
-	if (ret < 0)
+	char arr[] = "BIGMan";
+	int i = 0;
+	while (arr[i])
 	{
-		printf("arr1<arr2\n");
+		if (toupper(arr[i]))
+		{
+			arr[i] = tolower(arr[i]);
+		}
+		i++;
 	}
-	else if (ret > 0)
-	{
-		printf("arr1>arr2\n");
-	}
-	else
-	{
-		printf("arr1=arr2\n");
-	}
+	printf("%s\n", arr);
 	return 0;
 }
