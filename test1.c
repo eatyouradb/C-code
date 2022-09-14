a311dfe73b3fbd849d1ef6fd00c373e9c634f91d
G <108169022+eatyouradb@users.noreply.github.com>
2022-09-14 16:00:56 +0800

    实现memcpy功能的函数

--- test1.c
+++ test1.c
@@ -1,26 +1,29 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 #include <string.h>
+void* my_memcpy(void* dest, void* src, size_t count)
+{
+	char* ret = dest;
+	while (count--)
+	{
+		*(char*)dest = *(char*)src;
+		++(char*)dest;//dest++,但void类型不能++,须先强制转换成char*后++,
+		++(char*)src;//又因为++优先度高于(char*)，所以应先转换在++
+	}
+	return ret;
+}
 int main()
 {
-	char arr[] = "193@66#6.888";
-	char* p = "@#.";
-	char buf[1024] = { 0 };
-	strcpy(buf, arr);//备份
-
-	char* ret = NULL;
-	for (ret = strtok(arr, p); ret != NULL; ret = strtok(NULL, p))
+	char arr1[] = { 1,2,3,4,5 };
+	char arr2[10] = { 0 };
+	int i = 0;
+	int p = sizeof(arr2) / sizeof(arr2[0]);
+	/*memcpy(arr2, arr1, sizeof(arr1));*/
+	my_memcpy(arr2, arr1, sizeof(arr1));
+	for (i = 0; i < p; i++)
 	{
-		printf("%s\n", ret);
+		printf("%d\n", arr2[i]);
 	}
-
-	/*char *ret = strtok(arr, p);
-	printf("%s\n", ret);
-	ret = strtok(NULL, p);
-	printf("%s\n", ret);
-	ret = strtok(NULL, p);
-	printf("%s\n", ret);
-	ret = strtok(NULL, p);
-	printf("%s\n", ret);*/
+	
 	return 0;
 }
