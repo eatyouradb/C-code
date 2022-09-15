342e5c4dcb029c23e92908cc62c7ad4de32f9857
G <108169022+eatyouradb@users.noreply.github.com>
2022-09-15 17:03:00 +0800

    结构体传参练习

--- test1.c
+++ test1.c
@@ -1,29 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-#include <string.h>
-void* my_memcpy(void* dest, void* src, size_t count)
+struct s
 {
-	char* ret = dest;
-	while (count--)
-	{
-		*(char*)dest = *(char*)src;
-		++(char*)dest;//dest++,但void类型不能++,须先强制转换成char*后++,
-		++(char*)src;//又因为++优先度高于(char*)，所以应先转换在++
-	}
-	return ret;
+	char a;
+	int b;
+	double c;
+};
+void Init(struct s* x)
+{
+	x->a = 'w';
+	x->b = 100;
+	x->c = 3.14;
+}
+void print(struct s y)
+{
+	printf("%c %d %lf\n", y.a, y.b, y.c);
 }
 int main()
 {
-	char arr1[] = { 1,2,3,4,5 };
-	char arr2[10] = { 0 };
-	int i = 0;
-	int p = sizeof(arr2) / sizeof(arr2[0]);
-	/*memcpy(arr2, arr1, sizeof(arr1));*/
-	my_memcpy(arr2, arr1, sizeof(arr1));
-	for (i = 0; i < p; i++)
-	{
-		printf("%d\n", arr2[i]);
-	}
-	
+	struct s x = { 0 };
+	Init(&x);
+	print(x);
 	return 0;
 }
