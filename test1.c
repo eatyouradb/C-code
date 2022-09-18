aaa7f4a209fe542186316ee9cba3512337d9ad90
G <108169022+eatyouradb@users.noreply.github.com>
2022-09-18 16:56:40 +0800

    malloc练习

--- test1.c
+++ test1.c
@@ -1,30 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-struct s
-{
-	char a;
-	int b;
-	double c;
-};
-void Init(struct s* x)
-{
-	x->a = 'w';
-	x->b = 100;
-	x->c = 3.14;
-}
-void print1(struct s y)//传值
-{
-	printf("%c %d %lf\n", y.a, y.b, y.c);
-}
-void print2(const struct s* z)//传址
-{
-	printf("%c %d %lf\n", z->a, z->b, z->c);
-}
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 int main()
 {
-	struct s x = { 0 };
-	Init(&x);
-	print1(x);
-	print2(&x);
+	int* p = (int*)malloc(10 * sizeof(int));
+	if (NULL == p)
+	{
+		printf("%s\n", strerror(errno));
+	}
+	else
+	{
+		int i = 0;
+		for (i = 0; i < 10; i++)
+		{
+			*(p + i) = i;
+			printf("%d ", *(p + i));
+		}
+	}
+	
 	return 0;
 }
