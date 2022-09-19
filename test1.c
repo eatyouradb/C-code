6512d5e1b755b57364adb2744b51856a75723d8f
G <108169022+eatyouradb@users.noreply.github.com>
2022-09-19 20:42:54 +0800

    柔性数组练习

--- test1.c
+++ test1.c
@@ -1,23 +1,37 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 #include <stdlib.h>
-#include <errno.h>
 #include <string.h>
+#include <errno.h>
+struct S
+{
+	int n;
+	int arr[0];
+};
 int main()
 {
-	int* p = (int*)malloc(10 * sizeof(int));
-	if (NULL == p)
+	struct S s;
+	struct S* p = (struct S*)malloc(sizeof(struct S) + 5 * sizeof(int));
+	//假设arr内有5个int
+	p->n = 100;
+	int i = 0;
+	for (i = 0; i < 5; i++)
 	{
-		printf("%s\n", strerror(errno));
+		p->arr[i] = i;
 	}
-	else
+	//假设5个int不够,需要10个int
+	struct S* p1 = realloc(p, 44);//4+10*4
+	if (NULL != p1)
 	{
-		int i = 0;
-		for (i = 0; i < 10; i++)
-		{
-			*(p + i) = i;
-			printf("%d ", *(p + i));
-		}
+		p1 = p;
+	}
+	for (i = 5; i < 10; i++)
+	{
+		p1->arr[i] = i;
+	}
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", p1->arr[i]);
 	}
 	free(p);
 	p = NULL;
