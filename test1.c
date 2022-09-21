764be3671c5e1084434ae25c865239a065742970
G <108169022+eatyouradb@users.noreply.github.com>
2022-09-21 21:43:00 +0800

    格式化输出函数练习

--- test1.c
+++ test1.c
@@ -1,39 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
 struct S
 {
 	int n;
-	int arr[0];
+	float m;
+	char a[10];
 };
 int main()
 {
-	struct S s;
-	struct S* p = (struct S*)malloc(sizeof(struct S) + 5 * sizeof(int));
-	//假设arr内有5个int
-	p->n = 100;
-	int i = 0;
-	for (i = 0; i < 5; i++)
+	struct S s = { 10,3.14f,"bit" };
+	FILE* p = fopen("text.txt", "w");
+	if (NULL == p)
 	{
-		p->arr[i] = i;
+		return 0;
 	}
-	//假设5个int不够,需要10个int
-	struct S* p1 = realloc(p, 44);//4+10*4
-	if (NULL != p1)
-	{
-		p1 = p;
-	}
-	for (i = 5; i < 10; i++)
-	{
-		p1->arr[i] = i;
-	}
-	for (i = 0; i < 10; i++)
-	{
-		printf("%d ", p1->arr[i]);
-	}
-	free(p);
+	fprintf(p, "%d %f %s", s.n, s.m, s.a);
+
+	fclose(p);
 	p = NULL;
 	return 0;
 }
