07ea5950d7180f8f450e189780c9b2a682e46d5a
G <108169022+eatyouradb@users.noreply.github.com>
2022-09-25 21:39:09 +0800

    实现offsetof()功能的宏

--- test3.c
+++ test3.c
@@ -1,18 +1,17 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-#include <string.h>
+#define OFFSETOF(struct_name,member_name) (int)&(((struct_name*)0)->member_name)
+struct S
+{
+	char a;
+	int b;
+	char c;
+};
 int main()
 {
-	char a[1000];
-	int i = 0;
-	for (i = 0; i < 1000; i++)
-	{
-		a[i] = -1 - i;
-	}
-	//a为char类型，存放大小为-128——127
-	//数组a中的元素为-1,-2,-3,...,-1000
-	//当a[i]=-128时，下一元素-129应转化为-128——127中的数，即127,126,...,1,0,-1,...
-	//strlen遇到0停止，第一个0前有1——127+（-128）——（-1）个数（127+128）=255
-	printf("%d", strlen(a));
+	struct S s;
+	printf("%d\n", OFFSETOF(struct S, a));
+	printf("%d\n", OFFSETOF(struct S, b));
+	printf("%d\n", OFFSETOF(struct S, c));
 	return 0;
 }
