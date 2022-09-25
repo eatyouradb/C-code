20c88179972d223db83ac8de6b7df18ea6180e4c
G <108169022+eatyouradb@users.noreply.github.com>
2022-09-25 21:47:25 +0800

    #define定义宏的副作用

--- test2.c
+++ test2.c
@@ -1,22 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
-#define OFFSETOF(struct_name,member_name) (int)&(((struct_name*)0)->member_name)
-//0是地址，把它强制转换成struct_name*类型
-//用(struct_name*)0的地址找到成员a,b,c
-//((struct_name*)0)->member_name是变量，需要拿其地址
-//a/b/c的地址减去起始位置0得到偏移量->所以a/b/c的地址就是偏移量
-//偏移量是整型，&(((struct_name*)0)->member_name)强制转换成int
-struct S
-{
-	char a;
-	int b;
-	char c;
-};
+#define max(x,y) ((x)>(y)?(x):(y))
 int main()
 {
-	struct S s;
-	printf("%d\n", OFFSETOF(struct S, a));
-	printf("%d\n", OFFSETOF(struct S, b));
-	printf("%d\n", OFFSETOF(struct S, c));
-	return 0;
+	int a = 10;
+	int b = 11;
+	printf("%d\n", max(a++, b++));
+	//((a++)>(b++)?(a++):(b++))
+	//执行a>b,不大于,返回b++（12）
+	//然后a++(11),b++(13)
+	printf("%d\n", a);
+	printf("%d\n", b);
 }
